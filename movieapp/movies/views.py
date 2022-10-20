d99684084749f7b3af930e2f525437290f2bb3ae
Onur Yılmaz <onuryilmazpw@gmail.com>
2022-10-20 23:45:22 +0300

    add Urls & Views

--- movieapp/movies/views.py
+++ movieapp/movies/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("index")
+
+def movies(request):
+    return HttpResponse("movies")
+
+def movie_details(request, slug):
+    return HttpResponse("movie_details: " + slug)
